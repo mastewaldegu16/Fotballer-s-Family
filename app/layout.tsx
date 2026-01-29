f5225a74be6a3209e679102307119e1087eaa15b
mastewaldegu16 <mastewaldegu053@gmail.com>
2026-01-29 12:21:03 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,21 @@
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode
-}) {
+'use client'
+import { useRouter } from 'next/navigation'
+
+export default function Page() {
+  const router = useRouter()
+
   return (
-    <html>
-      <body style={{ fontFamily: 'Arial', padding: 20 }}>
-        <h1>⚽ Footballer's Family</h1>
-        {children}
-      </body>
-    </html>
+    <div>
+      <h2>Welcome to Footballer’s Family ⚽</h2>
+      <p>A community for football lovers.</p>
+
+      <button onClick={() => router.push('/login')}>
+        Login
+      </button>
+
+      <button onClick={() => router.push('/signup')}>
+        Signup
+      </button>
+    </div>
   )
 }
