7b80eac25939f1bb86a973b2b0359e620da826f1
mastewaldegu16 <mastewaldegu053@gmail.com>
2026-01-29 12:54:47 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,16 @@
-'use client'
-import { useRouter } from 'next/navigation'
-
-export default function Page() {
-  const router = useRouter()
-
+export default function HomePage() {
   return (
     <div>
       <h2>Welcome to Footballer’s Family ⚽</h2>
       <p>A community for football lovers.</p>
 
-      <button onClick={() => router.push('/login')}>
-        Login
-      </button>
+      <a href="/login">
+        <button>Login</button>
+      </a>
 
-      <button onClick={() => router.push('/signup')}>
-        Signup
-      </button>
+      <a href="/signup">
+        <button>Signup</button>
+      </a>
     </div>
   )
 }
