656263cfc51092012904f8f8a46f2a200b9a4130
mastewaldegu16 <mastewaldegu053@gmail.com>
2026-01-29 12:32:56 +0300

    Update and rename page.tsx to layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <body style={{ fontFamily: 'Arial', padding: 20 }}>
+        <h1>⚽ Footballer's Family</h1>
+        {children}
+      </body>
+    </html>
+  )
+}
